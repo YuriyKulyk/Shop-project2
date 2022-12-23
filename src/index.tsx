14d4e96278a8482a10d7f1e865b79676109f2fa2
yuriy <bipedyuriy@gmail.com>
2022-12-22 22:15:33 -1000

    function

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+function App() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore
+                consectetur rerum cum voluptatem fugiat possimus! Magni, enim,
+                non explicabo ut atque a autem deserunt, sequi et eius dolorem
+                consectetur placeat.
+            </p>
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
