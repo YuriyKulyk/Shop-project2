d56eb4dc0a4bfa664518487cb1d94d5884eebfb6
yuriy <bipedyuriy@gmail.com>
2022-12-22 22:22:29 -1000

    app + cont+title

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const App = () => {
+const Title = () => {
+    return <h1>Hello World</h1>
+}
+
+const Content = () => {
     return (
         <div>
-            <h1>Hello World</h1>
             <p>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore
                 consectetur rerum cum voluptatem fugiat possimus! Magni, enim,
                 non explicabo ut atque a autem deserunt, sequi et eius dolorem
                 consectetur placeat.
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Hic suscipit consequuntur, quod nostrum numquam laborum nisi
+                    repellat doloremque labore esse unde, nulla magnam
+                    assumenda, eveniet soluta fugiat reprehenderit facilis
+                    officiis.
+                </p>
             </p>
+        </div>
+    )
+}
+
+const App = () => {
+    return (
+        <div>
+            <Title />
+            <Content />
         </div>
     )
 }
