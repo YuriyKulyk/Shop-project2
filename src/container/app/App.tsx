9d8b85d24ddbef1c9abdc033e2524de945915fb0
yuriy <bipedyuriy@gmail.com>
2022-12-24 16:07:30 -1000

    header

--- src/container/app/App.tsx
+++ src/container/app/App.tsx
@@ -1,9 +1,11 @@
 import Header from 'container/Header/Header'
+import CssBaseline from '@mui/material/CssBaseline'
 
 type Props = {}
 const App = (props: Props) => {
     return (
         <>
+            <CssBaseline />
             <Header />
         </>
     )
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
+import App from 'container/App/App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
-        <>App</>
+        <App />
     </React.StrictMode>
 )
